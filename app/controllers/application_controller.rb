583456c178eff8f083ff5a5b955b8b298c0030e4
shunshun7676 <shunyamatsukata@gmail.com>
2022-12-01 22:42:03 +0900

    あ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :family_name, :first_name_kana, :family_name_kana, :birthday, :email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :family_name, :first_name_kana, :family_name_kana, :birthday])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "products#index" 
   resources :products do
-   resources :purchases, only: [:index, :create]
+   #resources :purchases, only: [:index, :create]
   end
  end
