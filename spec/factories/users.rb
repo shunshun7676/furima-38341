92e375dc9b24f732b502149c37f3744f478676e3
shunshun7676 <shunyamatsukata@gmail.com>
2022-12-02 21:01:52 +0900

    あ

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
  factory :user do
    nickname { Faker::Name.last_name }
    email { Faker::Internet.free_email }
-   password {'1a' Faker::Internet.password(min_length: 6) }
+   password {'1a'+ Faker::Internet.password(min_length: 6) }
    password_confirmation { password }
    first_name { Gimei.last.kanji }
    family_name { Gimei.first.kanji }
