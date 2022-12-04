e7eec7c830cb3580d1a4a0cdf21739597f205303
shunshun7676 <shunyamatsukata@gmail.com>
2022-12-05 00:38:52 +0900

    Active Storageの導入

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -47,8 +47,8 @@ class ProductsController < ApplicationController
   #  redirect_to user_session_path, alert: 'You need to sign in or sign up before continuing.' unless user_signed_in?
   #end
 
-  #def product_params
-  #  params.require(:product).permit(:image, :name, :explanation, :category_id, :derively_fee_id, :status_id, :prefecture_id, :day_id, :price).merge(user_id: current_user.id)
+  def product_params
+  params.require(:product).permit(:image, :name, :explanation, :category_id, :derively_fee_id, :status_id, :prefecture_id, :day_id, :price).merge(user_id: current_user.id)
   #end
 
   #def set_product
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ApplicationRecord
  belongs_to :user
  has_one :purchase
 # has_many :comments
+
+has_one_attached :image
+
 end
