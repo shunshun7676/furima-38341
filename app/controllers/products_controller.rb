00bcb9e0a1d6c2a27d7edcf87f9a72be02d46788
shunshun7676 <shunyamatsukata@gmail.com>
2022-12-19 15:21:02 +0900

    あ

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,23 +1,23 @@
 class ProductsController < ApplicationController
-  #before_action :require_login, only: :new, alert: 'You need to sign in or sign up before continuing.'
-  #before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :require_login, only: :new, alert: 'You need to sign in or sign up before continuing.'
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
-   # products = Product.all.order 
+  @products = Product.all.order('created_at DESC')
   end
 
-  #def new
-   #@product = Product.new
-  #end
+  def new
+  @product = Product.new
+  end
 
-  #ef create
-    #@product = Product.new(product_params)
-    #if @product.save
-      #redirect_to root_path
-   # else
-      #render :new
-    #end
-  #end
+  def create
+  @product = Product.new(product_params)
+  if @product.save
+  redirect_to root_path
+   else
+  render :new
+  end
+  end
 
   #def show
   #end
@@ -41,17 +41,17 @@ class ProductsController < ApplicationController
     #end
   #end
 
-#  private
+private
 
  # def require_login
   #  redirect_to user_session_path, alert: 'You need to sign in or sign up before continuing.' unless user_signed_in?
   #end
 
-  #def product_params
-  #  params.require(:product).permit(:image, :name, :explanation, :category_id, :derively_fee_id, :status_id, :prefecture_id, :day_id, :price).merge(user_id: current_user.id)
-  #end
+  def product_params
+  params.require(:product).permit(:image, :name, :explanation, :category_id, :derively_fee_id, :status_id, :prefecture_id, :day_id, :price).merge(user_id: current_user.id)
+  end
 
-  #def set_product
-   # @product = Product.find(params[:id])
-  #end
+  def set_product
+   @product = Product.find(params[:id])
+  end
 end
