79d8984714ac309931e81c46f476d15c45197d8c
shunshun7676 <shunyamatsukata@gmail.com>
2022-12-24 20:09:18 +0900

    商品詳細表示修正2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,7 +26,7 @@ class ProductsController < ApplicationController
 private
 
 def product_params
-    params.require(:product).permit(:image, :name, :explanation, :category_id, :status_id, :derively_fee_id, :prefecture_id, :hi_id, :price).merge(user_id: current_user.id)
+ params.require(:product).permit(:image, :name, :explanation, :category_id, :status_id, :derively_fee_id, :prefecture_id, :hi_id, :price).merge(user_id: current_user.id)
   end
 
   def set_product
