b107a1f842e4a69d4d9b946d0c0f8987ff3c667d
shunshun7676 <shunyamatsukata@gmail.com>
2023-06-05 23:03:46 +0900

    Update 20221120092127_devise_create_users.rb

--- db/migrate/20221120092127_devise_create_users.rb
+++ db/migrate/20221120092127_devise_create_users.rb
@@ -13,7 +13,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :encrypted_password, null: false, default: ""
       t.date   :birthday,          null: false
 
-      
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -39,11 +38,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
-      t.timestamps null: false
+   t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
+    add_index :users, :email,unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
